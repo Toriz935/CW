80696e6bd945652d1eb525e53bef05977faff7be
Chu <jesusatorizf@gmail.com>
2023-05-18 11:23:56 -0600

    añado la carpeta de la semana 2 y la tarea 6

---
+++ semana_2/actividad_6/dynamics/actividad_6PHP.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BoletosDonGerardo.com</title>
+    <link rel="icon" href="https://www.shutterstock.com/image-photo/middle-age-latin-man-wearing-260nw-2191637135.jpg" type="icon-x/image"></icon>
+</head>
+<body>
+    <h1><strong><u>Boletos:</u></strong></h1><br><br>
+    <?php
+        $nombre = (isset($_POST["nombre"]) && $_POST["nombre"] != "")? $_POST["nombre"] : "Falta nombre";//recibe "nombre" del formulario php
+        $apellido = (isset($_POST["apellido"]) && $_POST["apellido"] != "")? $_POST["apellido"] : "Faltan apellidos";//recibe "apellido" de php
+        $zona = (isset($_POST["zona"]) && $_POST["zona"] != "")? $_POST["zona"] : "Falta zona";//recibe "zona"
+        $num_boletos = (isset($_POST["num_boletos"]) && $_POST["num_boletos"] != "" )? $_POST["num_boletos"] :"Numero incorrecto de boletos";// recibe el número de boletos
+        $artista = (isset($_POST["artista"]) && $_POST["artista"] != "")? $_POST["artista"] : "Falta artista";//reibe "artista"
+        $fecha = (isset($_POST["fecha"]) && $_POST["fecha"] != "")? $_POST["fecha"]  : "Eliga una fecha";//recibe  "fecha"
+        $lugar = (isset($_POST["lugar"]) && $_POST["lugar"] != "")? $_POST["lugar"] : "Eliga un lugar";//recibe "lugar"
+        $extra_1 = (isset($_POST["extra_1"]) && $_POST["extra_1"] != "")? "Palomitas, " : "";// verifica si se agrego "extra_1", de no ser así en valor es nulo
+        $extra_2 = (isset($_POST["extra_2"]) && $_POST["extra_2"] != "")? "Refresco, " : "";
+        $extra_3 = (isset($_POST["extra_3"]) && $_POST["extra_3"] != "")? "VIP, " : "";
+        $extra_4 = (isset($_POST["extra_4"]) && $_POST["extra_4"] != "")? "Regalo " : "";
+        //Asignar una foto de acuerdo al artista elegido//////////////////////////////////////////////////////
+            //roberto
+        if(strcmp($artista, "Roberto Musso") == 0){
+            $foto_artista = "<img src='./../fotos/roberto.png' height='500px' width='500'>";
+            $frase_chida = "'<i>No soy un cordero a matar con cianuro, soy un guerrero y todavía respiro.</i>'";
+            //Cuarteto
+        }else
+        if(strcmp($artista, "El cuarteto de Nos") == 0){
+            $foto_artista = "<img src='./../fotos/cuarteto.png' height='500px' width='500'>";
+            $frase_chida = "'<i>Antes de dormirme hoy quiero afirmar que este un día más y no un día menos.</i>'";
+            
+        }else
+            //Taylor
+        if(strcmp($artista, "Taylor Swift") == 0){
+            $foto_artista = "<img src='./../fotos/taylor.png' height='500px' width='500'>";
+            $frase_chida = "'<i>Ninguna voz es tan cruel como la voz que cada uno de nosotros tiene en la cabeza.</i>'";
+            
+        }else
+            //Rosalía
+        if(strcmp($artista, "Rosalía") == 0){
+            $foto_artista = "<img src='./../fotos/rosalia.png' height='500px' width='500'>";
+            $frase_chida = "'<i>Si yo supiera compañero, que el sol que sale te ofende, con el sol me peleara.</i>'";
+        }else
+            //Chayanne
+        if(strcmp($artista, "Chayanne") == 0){
+                $foto_artista = "<img src='./../fotos/chayanne.png' height='500px' width='500'>";
+                $frase_chida = "'<i>Que se confundan la verdad y la mentira y que sólo tu amor ilumine mi vida.</i>'";
+            }
+///////////////////////////Asignar una foto dependiendo del lugar////////////////////////////////////////////////
+
+            //Auditorio Nacional
+        if(strcmp($lugar, "Auditorio Nacional") == 0){
+            $foto_lugar = "<img src='./../fotos/auditorio.png' height='250px' width='250'>";
+        }else 
+            //La explanada del Zócalo de la CDMX
+        if(strcmp($lugar, "La explanada del Zócalo de la CDMX") == 0){
+            $foto_lugar = "<img src='./../fotos/zocalo.png' height='250px' width='250'>";
+        }else
+            //La casa de Joaquín
+        if(strcmp($lugar, "La casa de Joaquín") == 0){
+            $foto_lugar = "<img src='./../fotos/casa.png' height='250px' width='250'>";
+        }else
+            //tepocueva
+        if(strcmp($lugar, "Tepocueva") == 0){
+            $foto_lugar = "<img src='./../fotos/tepocueva.png' height='250px' width='250'>";
+        }else
+            //Foro sol
+        if(strcmp($lugar, "Foro sol") == 0){
+            $foto_lugar = "<img src='./../fotos/foro.png' height='250px' width='250'>";
+        }
+////////////////////////Asignar una foto dependiedno de la zona///////////////////////////////////////////////////////////
+            //Gradas
+        if(strcmp($zona, "Gradas") == 0){
+            $foto_zona = "<img src='./../fotos/gradas.png' height='250px' width='250'>";
+        }else 
+            //Butacas
+        if(strcmp($zona, "Butacas") == 0){
+            $foto_zona = "<img src='./../fotos/butacas.png' height='250px' width='250'>";
+        }else 
+            //Balcón
+        if(strcmp($zona, "Balcón") == 0){
+            $foto_zona = "<img src='./../fotos/balcon.png' height='250px' width='250'>";
+        }else 
+            //De pie
+        if(strcmp($zona, "De pie") == 0){
+            $foto_zona = "<img src='./../fotos/de_pie.png' height='250px' width='250'>";
+        }else
+            //Hasta el frente
+        if(strcmp($zona, "Hasta el frente") == 0){
+            $foto_zona = "<img src='./../fotos/enfrente.png' height='250px' width='250'>";
+        }else
+            //Estacionamiento
+        if(strcmp($zona, "Estacionamiento") == 0){
+            $foto_zona = "<img src='./../fotos/estacionamiento.png' height='250px' width='250'>";
+        }
+        //////////////////////////////////////////
+        if($extra_1.$extra_2.$extra_3.$extra_4 != ""){
+            $extras = $extra_1.$extra_2.$extra_3.$extra_4;//junta los extras en una sola variable para imprimirlos con más sencillez, de no haber selecionado nada, guarda "ninguno"
+        }else{
+            $extras = "Ninguno";
+        }
+        if($num_boletos > 15){
+            $exceso = $num_boletos;//Asegura que solo se puedan imprimir 15 boletos.
+            $num_boletos = 15;
+        }else{
+            $exceso = 0;
+        }
+        $sobrante = $exceso - $num_boletos;
+        for($i=1;$i<=$num_boletos;$i++)
+        {
+            echo "
+                <table align='center' border='10' style='border-collapse: inherit;' cellpadding='15px'>
+                    <thead>
+                        <tr>
+                        <th colspan='4'><strong>Boletos para</strong> $artista</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><strong>Nombre:</strong> $nombre</td>
+                            <td><strong>Apellidos:</strong> $apellido</td>
+                            <td><strong>Fecha:</strong> $fecha</td>
+                            <td rowspan='4'><img src='./../fotos/codigo.png' alt='codigo de barras'></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Lugar:</strong> $lugar</td>
+                            <td>$foto_lugar</td>
+                            <td rowspan='2'>$foto_artista</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Zona:</strong> $zona</td>
+                            <td>$foto_zona</td>
+                        </tr>
+                        <tr>
+                            <td colspan='2'><strong>Extras:</strong>$extras</td>
+                            <td><i><strong>$frase_chida</strong></i></td>
+                        </tr>
+                    </body>
+                </table>
+            "."<br>"; 
+        }
+        if($sobrante > 0){
+            echo "<p align='center'><strong>Se imprimieron 15 boletos, faltaron $sobrante.</strong></p>";
+        }
+    ?>
+    <h1></h1>
+</body>
+</html>
